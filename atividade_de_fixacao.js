c95afa44e70b9c32c7cb875299a52fc407b9ea7d
MarceloSilva05 <jmarcelosilva.2021@gmail.com>
2021-09-16 20:47:32 -0300

    primeiro commit

---
+++ atividade_de_fixacao.js
@@ -0,0 +1,24 @@
+//1
+
+var tNumero=10;
+console.log(tNumero);
+//
+var texto="Olá";
+console.log(texto);
+//
+var valor=true;
+console.log(valor);
+//
+var carros=["Fiat Strada","Hyundai HB20","Fiat Argo"];
+console.log(carros);
+//
+var caneta={cor:"azul",preco:2.00,qtd:2};
+console.log(caneta);
+console.log(caneta.cor);
+//
+var semValorDefinido;
+console.log(semValorDefinido);
+//
+var valorVazio=null;
+console.log(valorVazio);
+
---
+++ atividade_de_fixacao2.js
@@ -0,0 +1,29 @@
+//2
+/*
+//1)
+var nome = "Marcelo";
+console.log(nome);
+
+var outroNome = "Neta";
+console.log(outroNome);
+//2)
+let preco = 2.50;
+preco=preco.toFixed(2);
+console.log(preco);
+
+let soma = 2+5;
+console.log(soma);
+//3)
+const cpf=12345678901;
+console.log(cpf);
+
+const dataNascimento=1997;
+console.log(dataNascimento);*/
+
+
+var x;
+if(x>=1){
+    x=+5
+
+}
+console.log(x)
